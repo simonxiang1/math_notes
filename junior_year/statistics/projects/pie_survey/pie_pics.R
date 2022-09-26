5b17148887c9e29da9efe982ba60f6254be28ea6
Simon Xiang <simonxiang@zohomail.com>
2022-09-26 13:22:24 -0500

    removed stuff not for the public eye

--- junior_year/statistics/projects/pie_survey/pie_pics.R
+++
@@ -1,6 +0,0 @@
-pies <- read.csv("pie_survey.csv")
-# pies - this command prints out the entire data set
-pie_table = table(pies$What.s.your.favorite.kind.of.pie.)
-pie_table
-pie(pie_table, col = c("red", "purple", "yellow")) #not pretty by default
-barplot(pie_table, col = c("blue", "brown", "orange"))
